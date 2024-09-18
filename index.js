14ed7d068281bddf9e1b2681a40b6dc9fe08f239
Vidhi Ajmera <113877644+Vidhi-Ajmera16355@users.noreply.github.com>
2024-09-18 14:47:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "*", // Temporarily allow all origins
   credentials: true,
 };
 app.use(cors(corsOptions));
